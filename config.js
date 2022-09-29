928b680098af677ddec8c6a1a239d80040261d6e
Maninderjit singh <maninder@anattadesign.com>
2022-09-29 23:12:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://api.openweathermap.org';
+export const API_URL = 'https://api.openweathermap.org';
 export const KEY = '4b97eae5b84d804b95857a2ee395de64';
 export const DATA_UNIT = 'metric';
