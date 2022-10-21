fd575edc43ca911f1b11a40a8bb015e7389700f8
iminiscript <maninder@anattadesign.com>
2022-10-21 12:24:55 +0530

    Fix API request

--- views/renderCity.js
+++ views/renderCity.js
@@ -5,14 +5,11 @@ class renderCity {
         this._data.location.forEach((item) => {
         const list = document.querySelector('.jsList'); 
         const weather = document.querySelector('.c-weather'); 
-        console.log(weather)
         const html = `
             <li class="c-listItem jsListItem" data-lat="${item.lat}" data-lon="${item.lon}">  ${item.name} | ${item.country}</li>` 
             list.insertAdjacentHTML('afterbegin', html);
             list.classList.remove('hidden');
-            if (weather.innerHTML !== null) {
-                weather.innerHTML = '';
-            }
+            weather.innerHTML = '';
             weather.classList.add('hidden');
         });
     }
